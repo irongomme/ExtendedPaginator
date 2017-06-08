ac7d1fcbd0bafbc20c8aae80a0b02af4581348ac
Jérôme DENIS <jerome.denis@e-wi.fr>
2017-06-08 15:39:41 +0200

    Remove useless comments and docs

--- src/Controller/Component/ExtendedPaginatorComponent.php
+++ src/Controller/Component/ExtendedPaginatorComponent.php
@@ -18,127 +18,17 @@ use Cake\Event\Event;
 class ExtendedPaginatorComponent extends PaginatorComponent
 {
     /**
-     * Default pagination settings.
-     *
-     * When calling paginate() these settings will be merged with the configuration
-     * you provide.
-     *
-     * - `maxLimit` - The maximum limit users can choose to view. Defaults to 100
-     * - `limit` - The initial number of items per page. Defaults to 20.
-     * - `page` - The starting page, defaults to 1.
-     * - `contain` - The associated models
-     * - `whitelist` - A list of parameters users are allowed to set using request
-     *   parameters. Modifying this list will allow users to have more influence
-     *   over pagination, be careful with what you permit.
-     *
-     * @var array
+     * @see \Cake\Controller\Component\PaginatorComponent::$_defaultConfig
      */
     protected $_defaultConfig = [
         'page' => 1,
         'limit' => 20,
         'maxLimit' => 100,
-        //'contain' => [],
-        'fields' => [],
         'whitelist' => ['limit', 'sort', 'page', 'contain', 'fields']
     ];
 
     /**
-     * Handles automatic pagination of model records.
-     *
-     * ### Configuring pagination
-     *
-     * When calling `paginate()` you can use the $settings parameter to pass in pagination settings.
-     * These settings are used to build the queries made and control other pagination settings.
-     *
-     * If your settings contain a key with the current table's alias. The data inside that key will be used.
-     * Otherwise the top level configuration will be used.
-     *
-     * ```
-     *  $settings = [
-     *    'limit' => 20,
-     *    'maxLimit' => 100
-     *  ];
-     *  $results = $paginator->paginate($table, $settings);
-     * ```
-     *
-     * The above settings will be used to paginate any Table. You can configure Table specific settings by
-     * keying the settings with the Table alias.
-     *
-     * ```
-     *  $settings = [
-     *    'Articles' => [
-     *      'limit' => 20,
-     *      'maxLimit' => 100
-     *    ],
-     *    'Comments' => [ ... ]
-     *  ];
-     *  $results = $paginator->paginate($table, $settings);
-     * ```
-     *
-     * This would allow you to have different pagination settings for `Articles` and `Comments` tables.
-     *
-     * ### Controlling sort fields
-     *
-     * By default CakePHP will automatically allow sorting on any column on the table object being
-     * paginated. Often times you will want to allow sorting on either associated columns or calculated
-     * fields. In these cases you will need to define a whitelist of all the columns you wish to allow
-     * sorting on. You can define the whitelist in the `$settings` parameter:
-     *
-     * ```
-     * $settings = [
-     *   'Articles' => [
-     *     'finder' => 'custom',
-     *     'sortWhitelist' => ['title', 'author_id', 'comment_count'],
-     *   ]
-     * ];
-     * ```
-     *
-     * Passing an empty array as whitelist disallows sorting altogether.
-     *
-     * ### Paginating with custom finders
-     *
-     * You can paginate with any find type defined on your table using the `finder` option.
-     *
-     * ```
-     *  $settings = [
-     *    'Articles' => [
-     *      'finder' => 'popular'
-     *    ]
-     *  ];
-     *  $results = $paginator->paginate($table, $settings);
-     * ```
-     *
-     * Would paginate using the `find('popular')` method.
-     *
-     * You can also pass an already created instance of a query to this method:
-     *
-     * ```
-     * $query = $this->Articles->find('popular')->matching('Tags', function ($q) {
-     *   return $q->where(['name' => 'CakePHP'])
-     * });
-     * $results = $paginator->paginate($query);
-     * ```
-     *
-     * ### Scoping Request parameters
-     *
-     * By using request parameter scopes you can paginate multiple queries in the same controller action:
-     *
-     * ```
-     * $articles = $paginator->paginate($articlesQuery, ['scope' => 'articles']);
-     * $tags = $paginator->paginate($tagsQuery, ['scope' => 'tags']);
-     * ```
-     *
-     * Each of the above queries will use different query string parameter sets
-     * for pagination data. An example URL paginating both results would be:
-     *
-     * ```
-     * /dashboard?articles[page]=1&tags[page]=2
-     * ```
-     *
-     * @param \Cake\Datasource\RepositoryInterface|\Cake\Datasource\QueryInterface $object The table or query to paginate.
-     * @param array $settings The settings/configuration used for pagination.
-     * @return \Cake\Datasource\ResultSetInterface Query results
-     * @throws \Cake\Network\Exception\NotFoundException
+     * @see \Cake\Controller\Component\PaginatorComponent::paginates($object, array $settings = [])
      */
     public function paginate($object, array $settings = [])
     {
@@ -217,21 +107,7 @@ class ExtendedPaginatorComponent extends PaginatorComponent
     }
 
     /**
-     * Validate that the desired sorting can be performed on the $object. Only fields or
-     * virtualFields can be sorted on. The direction param will also be sanitized. Lastly
-     * sort + direction keys will be converted into the model friendly order key.
-     *
-     * You can use the whitelist parameter to control which columns/fields are available for sorting.
-     * This helps prevent users from ordering large result sets on un-indexed values.
-     *
-     * If you need to sort on associated columns or synthetic properties you will need to use a whitelist.
-     *
-     * Any columns listed in the sort whitelist will be implicitly trusted. You can use this to sort
-     * on synthetic columns, or columns added in custom find operations that may not exist in the schema.
-     *
-     * @param \Cake\Datasource\RepositoryInterface $object Repository object.
-     * @param array $options The pagination options being used for this request.
-     * @return array An array of options with sort + direction removed and replaced with order if possible.
+     * @see \Cake\Controller\Component\PaginatorComponent::validateSort(RepositoryInterface $object, array $options)
      */
     public function validateSort(RepositoryInterface $object, array $options)
     {
